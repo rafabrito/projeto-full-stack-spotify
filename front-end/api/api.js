bb395bcdaa344e795de3e8f33b33145993cdeecc
Rafael <rafa.brito295@gmail.com>
2025-02-19 13:49:16 -0300

    Readjust for Deploy and Removal of Dotenv

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,12 +1,11 @@
 // Fatch: é nativo do JavaScript
 // Axios: o axios é uma biblioteca
 import axios from "axios";
-import "dotenv/config";
 
 const { NODE_ENV } = process.env;
 
-// const URL = "http://localhost:3001/api";
-const URL = "https://projeto-full-stack-spotify.onrender.com/";
+const URL = "http://localhost:3001/api";
+// const URL = "https://projeto-full-stack-spotify.onrender.com/";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
